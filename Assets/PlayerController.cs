242f819840e633c80a1a386474d6beb40fa95e94
RisanStar <nBuckley2023@elam.co.uk>
2023-09-22 12:44:05 +0100

    Movement
    Made the Cube move

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject groundchecker;
+    public LayerMask WhatIsGround;
+
+    public float maxSpeed = 10.0f;
+    bool IsOnGround = false;
+    // Ablew to manipulate the body
+    Rigidbody2D playerobject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Attaches Code to body
+        playerobject = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Horizontal and Vertical input
+        float movementValueX = Input.GetAxis("Horizontal");
+        
+
+        //Velocity
+        playerobject.velocity = new Vector2(movementValueX*maxSpeed, playerobject.velocity.y);
+
+        IsOnGround = Physics2D.OverlapCircle(groundchecker.transform.position, 1.0f, WhatIsGround);
+    }
+}
