0cf0fdffad023cb782a85249b96a9289a7814d16
RisanStar <nBuckley2023@elam.co.uk>
2023-09-29 12:44:44 +0100

    Jump and camera
    Added camera movement and jump function

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 
@@ -8,7 +9,7 @@ public class PlayerController : MonoBehaviour
     public GameObject groundchecker;
     public LayerMask WhatIsGround;
 
-    public float maxSpeed = 10.0f;
+    public float maxSpeed = 15.0f;
     bool isOnGround = false;
     // Ablew to manipulate the body
     Rigidbody2D playerobject;
@@ -24,13 +25,29 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Horizontal and Vertical input
-        float movementValueX = Input.GetAxis("Horizontal");
-        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            maxSpeed = 13.0f;
+        }
+        else
+        {
+            maxSpeed = 5.0f;
+        }
+
+        float movementValueX = 2.0f;
+        float movementValueY = 2.0f;
+
 
         //Velocity
-        playerobject.velocity = new Vector2(movementValueX*maxSpeed, playerobject.velocity.y);
+        playerobject.velocity = new Vector2(movementValueX * maxSpeed, playerobject.velocity.y);
+
 
         isOnGround = Physics2D.OverlapCircle(groundchecker.transform.position, 1.0f, WhatIsGround);
-       
+        if (isOnGround)
+        {
+            if (Input.GetKey(KeyCode.Space))
+                playerobject.velocity = new Vector3(movementValueY * maxSpeed, playerobject.velocity.x);
+        }
+
     }
 }
